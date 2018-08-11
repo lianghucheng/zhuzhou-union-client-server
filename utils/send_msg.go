8c3ba12567b929dcaa013e5a8f4af045035e482a
lianghucheng <1123323039@qq.com>
2018-08-11 18:32:15 +0800

    发送短信

--- utils/send_msg.go
+++ utils/send_msg.go
@@ -7,11 +7,12 @@ import (
 
 // 发送短信
 func SendMsg(username string){
-	clnt := ypclnt.New(beego.AppConfig.String("yun_pian_apikey"))
+	clnt := ypclnt.New("037ced20fb015ee88829cd3e6248aa6f")
 	param := ypclnt.NewParam(2)
 	param[ypclnt.MOBILE] = username
 	code:=GetCode(username)
-	param[ypclnt.TEXT] = beego.AppConfig.String("msg_template")+code
+	param[ypclnt.TEXT] = "【易正网络】您的验证码是"+code
+	param[ypclnt.SIGN]="【易正网络】"
 	r := clnt.Sms().SingleSend(param)
 	beego.Debug(r)
 }
