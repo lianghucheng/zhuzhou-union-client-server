5f2634c3ce44177ce2b92c246f7ae11ebc46285f
mac <haoxiong@gmail.com>
2018-08-16 15:21:46 +0800

    后端公共部分

--- controllers/common.go
+++ controllers/common.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"github.com/astaxie/beego"
 	"zhuzhou-union-client-server/models"
+	"fmt"
 )
 
 type Common struct {
@@ -17,14 +18,62 @@ type CommonController struct {
 }
 
 func (this *Common) Prepare() {
-	var user models.User
 
-	user.Name = "test"
+	var code models.QrCode
 
-	this.Userinfo = &user
-	if this.Userinfo != nil {
-		this.Data["user"] = this.Userinfo
+	user := models.User{
+		Username: "test",
 	}
+	var Menus []*models.Menu
+	if err := models.DB.Preload("Category").Find(&Menus).Error; err != nil {
+		beego.Error("查询菜单错误", err)
+	}
+
+	var outputMenus []models.Menu
+	for _, menu := range Menus {
+		var outputMenu models.Menu
+		if menu.CategoryID != 0 {
+			category := menu.Category
+
+			var categoryMenu models.Menu
+			categoryMenu.Name = category.Name
+			categoryMenu.URL = "/category/" + fmt.Sprintf("%s", category.ID)
+			categoryMenu.Sequence = category.Sequence
+			var subCategorys []*models.Category
+			if err := models.DB.
+				Where("higher_id = ?", category.ID).
+				Find(&subCategorys).Error; err != nil {
+				beego.Error("查询子菜单错误")
+			}
+
+			for _, subCategory := range subCategorys {
+				var itemMenu models.Menu
+				itemMenu.Name = subCategory.Name
+				itemMenu.URL = "/category/" + fmt.Sprintf("%s", subCategory.ID)
+				itemMenu.Sequence = subCategory.Sequence
+				categoryMenu.Menus = append(categoryMenu.Menus, itemMenu)
+			}
+			outputMenu = categoryMenu
+
+		} else {
+			var notCategoryMenu models.Menu
+			notCategoryMenu.Name = menu.Name
+			notCategoryMenu.URL = menu.URL
+			notCategoryMenu.Sequence = menu.Sequence
+			outputMenu = notCategoryMenu
+		}
+		outputMenus = append(outputMenus, outputMenu)
+	}
+	if err := models.DB.First(&code).Error; err != nil {
+		beego.Error("没有发现二维码")
+	}
+	this.SetSession("userinfo", user)
+	user, ok := this.GetSession("userinfo").(models.User)
+	if ok {
+
+		this.Data["user"] = user
+	}
+	this.Data["Code"] = code
 }
 
 func (this *Common) UserFilter() {
