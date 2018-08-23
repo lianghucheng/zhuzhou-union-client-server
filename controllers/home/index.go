c97258176d42d8535486a05c0ba53951f0a72706
dadao <474068077@qq.com>
2018-08-23 10:13:05 +0800

    新闻内页

--- controllers/home/index.go
+++ controllers/home/index.go
@@ -5,12 +5,29 @@ import (
 	"zhuzhou-union-client-server/models"
 )
 
+func (this *Controller) LoadNews() {
+
+	var politicalNews []models.Article
+	var unionNews []models.Article
+	var grassrootsNews []models.Article
+	var wechatNews []models.Article
+
+	models.DB.Where("category_id=?", 230).Order("created_at desc").Limit(6).Find(&politicalNews)
+	models.DB.Where("category_id=?", 231).Order("created_at desc").Limit(6).Find(&unionNews)
+	models.DB.Where("category_id=?", 233).Order("created_at desc").Limit(6).Find(&grassrootsNews)
+	models.DB.Where("category_id=?", 232).Order("created_at desc").Limit(6).Find(&wechatNews)
+
+	this.Data["politicalNews"] = politicalNews
+	this.Data["unionNews"] = unionNews
+	this.Data["grassrootsNews"] = grassrootsNews
+	this.Data["wechatNews"] = wechatNews
+}
+
 //@router /	[*]
 func (this *Controller) Index() {
+
 	var homes []*models.Home
-
 	var rotation []*models.Rotation
-
 	var indexPer int
 
 	indexPer, _ = beego.AppConfig.Int("indexPer")
@@ -18,97 +35,23 @@ func (this *Controller) Index() {
 		indexPer = 5
 	}
 
-	//首页文章
-	if err := models.DB.
-		Preload("Category").
-		Find(&homes).Error; err != nil {
-		beego.Error("获取首页表数据错误", err)
-		this.Abort("500")
-		return
-	}
-
-	if err := models.DB.
-		Find(&rotation).Order("sequence asc").Error; err != nil {
-		beego.Error("获取首页轮播图错误", err)
-	}
-
-	outputIndex := make([]map[string]interface{}, 0)
-
-	//首页子分类
-	for _, h := range homes {
-		var articles1 []*models.Article
-		subCatesM := make([]map[string]interface{}, 0)
-		a := make(map[string]interface{})
-		var subCategorys []*models.Category
-		if h.Position == 1 && h.Layout == 1 {
-			if err := models.DB.
-				Where("higher_id = ?", h.CategoryID).
-				Find(&subCategorys).Error; err != nil {
-				beego.Error("读取首页子分类错误", err)
-				this.Abort("500")
-				return
-			}
-
-			for _, subCategory := range subCategorys {
-				var articles []*models.Article
-				subCateM := make(map[string]interface{})
-
-				if err := models.DB.
-					Where("category_id = ? and status =?", subCategory.ID, 1).Limit(indexPer).
-					Find(&articles).Error; err != nil {
-					beego.Error("读取首页子分类错误", err)
-					this.Abort("500")
-					return
-				}
-				subCateM["ID"] = subCategory.ID
-				subCateM["Name"] = subCategory.Name
-				subCateM["HigherID"] = subCategory.HigherID
-				subCateM["Sequence"] = subCategory.Sequence
-				subCateM["CreatedAt"] = subCategory.CreatedAt
-				subCateM["DeletedAt"] = subCategory.DeletedAt
-				subCateM["UpdatedAt"] = subCategory.UpdatedAt
-				subCateM["Category"] = subCategory.Category
-				subCateM["Articles"] = articles
-				subCatesM = append(subCatesM, subCateM)
-			}
-		}
-
-		if h.Position == 13 || h.Position == 14 {
-			indexPer = 5
-		}
-
-		if h.Position == 15 || h.Position == 16 {
-			indexPer = 10
-		}
-		if err := models.DB.
-			Where("category_id = ? and status = ?", h.CategoryID, 1).Limit(indexPer).
-			Find(&articles1).Error; err != nil {
-			this.Abort("500")
-			return
-		}
-
-		a["Articles"] = articles1
-		a["Home"] = h
-		a["SubCates"] = subCatesM
-		outputIndex = append(outputIndex, a)
-	}
+	models.DB.Order("sequence asc").Find(&rotation)
 	//首页底部图片链接
 	var imageLinks []*models.ImageLinks
 	if err := models.DB.Limit(5).Find(&imageLinks).Error; err != nil {
 		beego.Error("获取首页图片链接错误", err)
 	}
-
 	//首页底部下拉框链接
 	var boxLinks []*models.BoxLinks
-
 	if err := models.DB.Find(&boxLinks).Error; err != nil {
 		beego.Error("获取首页下拉链接错误", err)
 	}
 
+	this.LoadNews()
+
 	this.Data["rotations"] = rotation
 	this.Data["imageLinks"] = imageLinks
 	this.Data["homes"] = homes
-	this.Data["output"] = outputIndex
 	this.Data["boxLinks"] = boxLinks
 	this.TplName = "web/index.html"
 }
