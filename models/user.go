7d8fe395dcc4e9e4515214761c1356ee27cf36bf
haoxiongxiao <269812428@qq.com>
2018-08-28 10:39:42 +0800

    debug user

--- models/user.go
+++ models/user.go
@@ -7,7 +7,7 @@ type User struct {
 	Username string `json:"username" gorm:"unique"`
 	Password string `json:"password"`
 	Prioty   int	//1,root管理员   2,普通管理员	2,用户
-	Name     string
+	NickName     string
 	Sex      int //1男   2女
 	Icon     string
 	QQ       string
